4f455491f7387115b0e5139de2c09019f88cdf64
Владислав <VladislavVyt@yandex.ru>
2024-03-25 21:43:05 +0300

    corrections 3.6

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,8 +1,8 @@
 import os
 from pathlib import Path
+from pathlib import Path
 
 from dotenv import load_dotenv
-from pathlib import Path
 
 dotenv_path = Path('/.env')
 load_dotenv(dotenv_path=dotenv_path)
